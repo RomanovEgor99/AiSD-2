4f80c1063c4023e10645e97b3924db085b614845
RomanovEgor99 <romanov.egor2010.ru@gmail.com>
2020-04-13 11:27:42 +0400

    a

--- AiSD 2/Program.cs
+++ AiSD 2/Program.cs
@@ -9,7 +9,24 @@ namespace AiSD_2
     class Program
     {
         static void Main(string[] args)
-        {
+        {   Console.WriteLine("Введите Фамилию студента:");
+            string lastName = Console.ReadLine();
+            Char[] arrLastName;
+            arrLastName = lastName.ToCharArray(0,lastName.Length);
+
+            Console.WriteLine("Введите имя студента");
+            string firstName = Console.ReadLine();
+            Char[] arrFirstName;
+            arrFirstName = firstName.ToCharArray(0, firstName.Length);
+
+            Console.WriteLine("Введите id студента:");
+            int studentId = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
         }
     }
 }
